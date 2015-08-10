93be34cce8d81d5b8a89df7bce5126ad0dc26b52
mbeissinger <mbeissinger@gmail.com>
2015-08-10 14:12:05 -0700

    Updated with examples

--- ner.py
+++ ner.py
@@ -3,26 +3,18 @@ Character-level recurrent model for named entity recognition.
 """
 from __future__ import print_function, division
 # normal imports
-import math
-import itertools
+import string
 import numpy
-import theano.tensor as T
 import pickle
 # opendeep imports
-import opendeep
-from opendeep.data import NumpyDataset
 from opendeep.models import LSTM
-from opendeep.monitor import Monitor, Plot
-from opendeep.optimization import RMSProp
 from opendeep.utils.misc import numpy_one_hot
-# internal imports
-# from data_processing import parse_text_files
 
 
-class OpenNER():
+class OpenNER:
     vocab_file = 'vocab.pkl'
     entity_vocab_file = 'entity_vocab.pkl'
-    lstm_file = 'trained_lstm.pkl'
+    lstm_file = 'trained_epoch_94.pkl'
 
     def __init__(self):
         self.vocab = pickle.load(open(self.vocab_file, 'rb'))
@@ -31,10 +23,8 @@ class OpenNER():
         self.inverse_entity_vocab = {v: k for k, v in self.entity_vocab.items()}
 
         # define our model! we are using lstm.
-        hidden_size = 128
-
         self.lstm = LSTM(input_size=len(self.vocab),
-                         hidden_size=hidden_size,
+                         hidden_size=128,
                          output_size=len(self.entity_vocab),
                          hidden_activation='tanh',
                          inner_hidden_activation='sigmoid',
@@ -51,27 +41,46 @@ class OpenNER():
 
         self.lstm.load_params(self.lstm_file)
 
-    # if you want debugging output from opendeep
-    opendeep.config_root_logger()
+        # # if you want debugging output from opendeep
+        # opendeep.config_root_logger()
 
     @staticmethod
-    def __get_entities(data, predictions, vocab, entity_vocab):
+    def __get_entities(data, predictions, vocab_inv, entity_vocab_inv, entity_vocab):
         # find contiguous entity characters across timesteps
+        non_entity_label = entity_vocab.get('O')
         entities = []
-        for i, batch in enumerate(predictions):
-            previous_label = 0
-            continuous_string = ""
-            for j, label in enumerate(batch):
-                # if not continuous, reset
-                if label != previous_label:
-                    entity = continuous_string
-                    # add only if the label is an entity
-                    if previous_label != 0:
-                        label_string = entity_vocab.get(previous_label)
-                        entities.append((entity, label_string))
-                    continuous_string = ""
-                data_char = vocab.get(numpy.argmax(data[i, j]))
-                continuous_string += data_char
+        for i, query in enumerate(predictions):
+            previous_label = non_entity_label
+            entity_string = ""
+            for j, label in enumerate(query):
+                # find entity start point (expand to space character) and extract the continuous entity
+                if label != non_entity_label and label != previous_label:
+                    entity_start = j
+                    while vocab_inv.get(numpy.argmax(data[i, entity_start])) not in string.whitespace and entity_start >= 0:
+                        entity_start -= 1
+                    # move start point forward one to get out of whitespace or back to 0 index
+                    entity_start += 1
+                    # now from the start point, extract continuous until whitespace or punctuation
+                    entity_idx = entity_start
+                    while entity_idx < len(query) and \
+                            (
+                                query[entity_idx] == label or
+                                entity_idx == entity_start or
+                                (
+                                    entity_idx > entity_start and
+                                    vocab_inv.get(numpy.argmax(data[i, entity_idx])) not in string.whitespace + string.punctuation and
+                                    vocab_inv.get(numpy.argmax(data[i, entity_idx-1])) not in string.whitespace + string.punctuation
+                                )
+                            ):
+                        entity_string += vocab_inv.get(numpy.argmax(data[i, entity_idx]))
+                        entity_idx += 1
+                    # get rid of trailing matched punctuation
+                    if entity_string[-1] in string.punctuation:
+                        entity_string = entity_string[:-1]
+                    # add the entity stripped of whitespace in beginning and end, and reset the string
+                    entities.append(entity_string.strip())
+                    entity_string = ""
+
                 previous_label = label
 
         return entities
@@ -102,7 +111,7 @@ class OpenNER():
         # now extract the guessed entities
         predictions = numpy.argmax(character_probs, axis=2)
 
-        entities = self.__get_entities(data, predictions, self.inverse_vocab, self.inverse_entity_vocab)
+        entities = self.__get_entities(data, predictions, self.inverse_vocab, self.inverse_entity_vocab, self.entity_vocab)
 
         return entities
 
@@ -115,5 +124,7 @@ class OpenNER():
 
 if __name__ == "__main__":
     ner = OpenNER()
-    yhat = ner.predict("iphone 6 is a pretty cool phone")
-    print(yhat)
+    print(ner.predict("does the iphone 6 have good battery compared to the galaxy s6?"))
+    print(ner.predict("iphone 8 is a pretty cool phone"))
+    print(ner.predict("I'm loving this new iphone 1200!!"))
+    print(ner.predict(" this iphone is the most fun phone ever made"))
